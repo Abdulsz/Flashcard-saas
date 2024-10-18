bd8cecabb6727624cb1d80d7ddd3721b8c457afe
Abdul-Salam Zakaria <151104581+Abdulsz@users.noreply.github.com>
2024-10-17 21:46:14 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC9WZYG8rWf-y-jwPTqVvDzntNsF-PDbc8",
-  authDomain: "flashcard-saas-7047c.firebaseapp.com",
-  projectId: "flashcard-saas-7047c",
-  storageBucket: "flashcard-saas-7047c.appspot.com",
-  messagingSenderId: "556071636600",
-  appId: "1:556071636600:web:28459c7ac28684c3aa678d",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
